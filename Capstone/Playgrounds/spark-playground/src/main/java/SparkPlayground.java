3cbaac422850068bdf047c670ef7abe73be4ca94
Matt Josse <matt.m.josse@gmail.com>
2018-09-13 14:53:01 -0600

    removed the tutorial routes

--- Capstone/Playgrounds/spark-playground/src/main/java/SparkPlayground.java
+++ Capstone/Playgrounds/spark-playground/src/main/java/SparkPlayground.java
@@ -24,57 +24,5 @@ public class SparkPlayground {
       return "";
     });
 
-    final UserService userService = new UserServiceMap();
-
-    post("/users", (req, res) -> {
-      res.type("application/json");
-
-      User user = new Gson().fromJson(req.body(), User.class);
-      userService.addUser(user);
-
-      return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUsers())));
-    });
-
-    get("/users", (req, res) -> {
-      res.type("application/json");
-      return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService.getUsers())));
-    });
-
-    get("/users/:id", (req, res) -> {
-      res.type("application/json");
-      return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(userService
-              .getUser(req.params(":id")))));
-    });
-
-    put("/users/:id", (req, res) -> {
-      res.type("application/json");
-
-      User toEdit = new Gson().fromJson(req.body(), User.class);
-      User editedUser = userService.editUser(toEdit);
-
-      String result;
-      if (editedUser != null) {
-        result = new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, new Gson().toJsonTree(editedUser)));
-      } else {
-        result = new Gson().toJson(new StandardResponse(StatusResponse.ERROR, new Gson().toJson("User not found or " +
-                "error in edit")));
-      }
-
-      return result;
-    });
-
-    delete("/users/:id", (req, res) -> {
-      res.type("application/json");
-
-      userService.deleteUser(req.params(":id"));
-      return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, "user deleted"));
-    });
-
-    options("/users/:id", (req, res) -> {
-      res.type("application/json");
-      return new Gson().toJson(new StandardResponse(StatusResponse.SUCCESS, (userService.userExists(req.params
-              (":id"))) ? "User exists" : "User does not exist"));
-    });
-
   }
 }
