abc0769241942cf85b557bd87fb05b35747f61d9
Matt Josse <matt.m.josse@gmail.com>
2018-10-05 01:30:13 -0600

    minor refactoring and updated javadocs

--- Capstone/FamilyGraph/src/main/java/FamilyGraph.java
+++ Capstone/FamilyGraph/src/main/java/FamilyGraph.java
@@ -8,8 +8,142 @@ public class FamilyGraph {
   private HashMap<Integer, Person> personMap = new HashMap<>(); // Person.id : Person
   private HashMap<Integer, Family> familyMap = new HashMap<>(); // Family.id : Family
 
-  public FamilyGraph() {
-    // Not implemented.
+
+  /**
+   * Adds the specified {@link Person} to the graph, if not already present. In other words, if the graph does not
+   * contain a Person x, such that x.id == p.id, p is added to the graph and this method returns true.
+   *
+   * @param p Person to be added to the graph.
+   * @return true if the graph does not already contain the id of p.
+   */
+  public boolean addPerson(Person p) {
+    int id = p.getId();
+    if (personMap.containsKey(id)) {
+      return false;
+    }
+    personMap.put(id, p);
+    return true;
+  }
+
+  /**
+   * Returns the {@link Person} as specified by the id argument, if contained in the graph. In other words, if the
+   * graph contains Person p, such that p.id == id, p is returned; otherwise, null.
+   *
+   * @param id id of the Person to be retrieved from the graph.
+   * @return the Person as specified by the id argument, if contained in the graph, null otherwise.
+   */
+  public Person getPerson(int id) {
+    return personMap.get(id);
+  }
+
+  /**
+   * Adds the specified {@link Family} to the graph, if not already present. In other words, if the graph does not
+   * contain a Family x, such that x.id == f.id, f is added to the graph and this method returns true.
+   *
+   * @param f Family to be added to the graph.
+   * @return true if the graph does not already contain the id of f.
+   */
+  public boolean addFamily(Family f) {
+    int id = f.getId();
+    if (familyMap.containsKey(id)) {
+      return false;
+    }
+    familyMap.put(id, f);
+    return true;
+  }
+
+  /**
+   * Returns the {@link Family} as specified by the id argument, if contained in the graph. In other words, if the
+   * graph contains Family f, such that f.id == id, f is returned; otherwise, null.
+   *
+   * @param id id of the Family to be retrieved from the graph.
+   * @return the Family as specified by the id argument, if contained in the graph, null otherwise.
+   */
+  public Family getFamily(int id) {
+    return familyMap.get(id);
+  }
+
+  public boolean addChildToFamily(Person child, Family f) {
+    if (!personMap.containsKey(child.getId()) || !familyMap.containsKey(f.getId())) {
+
+      /* TODO
+       * Throw Exception or return false? Returning false is more consistent with the other methods in this class and
+       * other libraries I've seen/used. However, this provides no user-feedback as to what went wrong. For example, the
+       * Person, the Family, or both may not have existed in the graph before this method call was made.
+       */
+      throw new IllegalArgumentException();
+    }
+
+    addChildEdge(child, f);
+
+    return true;
+  }
+
+  public boolean addParentToFamily(Person parent, Family f) {
+    if (!personMap.containsKey(parent.getId()) || !familyMap.containsKey(f.getId())) {
+      throw new IllegalArgumentException();
+    }
+
+    int nParents = f.nParents;
+    if (nParents >= f.MAX_PARENTS) {
+      return false;
+    }
+    int index = nParents == 0 ? 0 : 1;
+    addParentEdge(parent, f, index);
+    return true;
+  }
+
+  private void addChildEdge(Person child, Family f) {
+    f.addChild(child);
+    child.addChildFamily(f);
+  }
+
+  private void addParentEdge(Person parent, Family f, int index) {
+    f.addParent(parent, index);
+    parent.addParentFamily(f);
+  }
+
+  /**
+   * Retrieves the direct-line ancestry of the {@link Person} specified by the root argument. Root is also returned as
+   * the first item in the ArrayList. In other words, if the graph contains Person p, such that p.id == root.id, an
+   * ArrayList containing at least root is guaranteed to be returned.
+   *
+   * @param root Person for whom the direct-line ancestry is to be returned (the starting point).
+   * @return the direct-line ancestry of root; possibly resulting in only root itself.
+   */
+  public ArrayList<Person> getDirectLineAncestry(Person root) {
+    if (!personMap.containsKey(root.getId())) {
+      return null;
+    }
+
+    ArrayList<Person> directLine = new ArrayList<>();
+    directLine.add(root);
+    recurseDirectLine(root, directLine);
+
+    return directLine;
+  }
+
+  private void recurseDirectLine(Person root, ArrayList<Person> directLine) {
+
+    /* TODO
+     * Refactor.
+     */
+
+    Family childFamily = root.childFamily;
+    if (childFamily == null) {
+      return;
+    }
+
+    Person[] parents = childFamily.parents;
+    Person parentA = parents[0];
+    Person parentB = parents[1];
+
+    if (parentA != null) { directLine.add(parentA); }
+    if (parentB != null) { directLine.add(parentB); }
+
+    if (parentA != null) { recurseDirectLine(parentA, directLine); }
+    if (parentB != null) { recurseDirectLine(parentB, directLine); }
+
   }
 
   public static class Person {
@@ -71,104 +205,5 @@ public class FamilyGraph {
       children.add(child);
     }
   } // End Family
-
-  public boolean addPerson(Person p) {
-    int id = p.getId();
-    if (personMap.containsKey(id)) {
-      return false;
-    }
-    personMap.put(id, p);
-    return true;
-  }
-
-  public Person getPerson(int id) {
-    return personMap.get(id);
-  }
-
-  public boolean addFamily(Family f) {
-    int id = f.getId();
-    if (familyMap.containsKey(id)) {
-      return false;
-    }
-    familyMap.put(id, f);
-    return true;
-  }
-
-  public Family getFamily(int id) {
-    return familyMap.get(id);
-  }
-
-  public boolean addChildToFamily(Person child, Family f) {
-    if (!personMap.containsKey(child.getId()) || !familyMap.containsKey(f.getId())) {
-
-      /* TODO
-       * Throw Exception or return false? Returning false is more consistent with the other methods in this class and
-       * other libraries I've seen/used. However, this provides no user-feedback as to what went wrong. For example, the
-       * Person, the Family, or both may not have existed in the graph before this method call was made.
-       */
-      throw new IllegalArgumentException();
-    }
-
-    // Add a bidirectional edge.
-    f.addChild(child);
-    child.addChildFamily(f);
-
-    return true;
-  }
-
-  public boolean addParentToFamily(Person parent, Family f) {
-    if (!personMap.containsKey(parent.getId()) || !familyMap.containsKey(f.getId())) {
-      /* TODO
-       * See addChildToFamily for a similar concern.
-       */
-      throw new IllegalArgumentException();
-    }
-
-    int nParents = f.nParents;
-    if (nParents >= f.MAX_PARENTS) {
-      return false;
-    }
-    int index = nParents == 0 ? 0 : 1;
-    // Add a bidirectional edge.
-    f.addParent(parent, index);
-    parent.addParentFamily(f);
-
-    return true;
-  }
-
-  public ArrayList<Person> getDirectLineAncestry(Person root) {
-    if (!personMap.containsKey(root.getId())) {
-      return null;
-    }
-
-    ArrayList<Person> directLine = new ArrayList<>();
-    directLine.add(root);
-    recurseDirectLine(root, directLine);
-
-    return directLine;
-  }
-
-  private void recurseDirectLine(Person root, ArrayList<Person> directLine) {
-
-    /* TODO
-     * Refactor.
-     */
-
-    Family childFamily = root.childFamily;
-    if (childFamily == null) {
-      return;
-    }
-
-    Person[] parents = childFamily.parents;
-    Person parentA = parents[0];
-    Person parentB = parents[1];
-
-    if (parentA != null) { directLine.add(parentA); }
-    if (parentB != null) { directLine.add(parentB); }
-
-    if (parentA != null) { recurseDirectLine(parentA, directLine); }
-    if (parentB != null) { recurseDirectLine(parentB, directLine); }
-
-  }
 
 } // End FamilyGraph
